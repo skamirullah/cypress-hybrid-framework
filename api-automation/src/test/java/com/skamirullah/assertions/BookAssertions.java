6410518459eb86ce45599ad9e5016c7c8c1fbe21
skamirullah <amirthoughts@gmail.com>
2025-12-17 18:31:42 +0530

    added screenshots

--- api-automation/src/test/java/com/skamirullah/assertions/BookAssertions.java
+++ api-automation/src/test/java/com/skamirullah/assertions/BookAssertions.java
@@ -5,29 +5,39 @@ import org.testng.Assert;
 import com.skamirullah.constants.ApiMessages;
 import com.skamirullah.models.response.AddBookResponse;
 
-import io.qameta.allure.Step;
+import io.qameta.allure.Allure;
 import io.restassured.response.Response;
 
 public class BookAssertions {
 
-    @Step("Validate status code is 200")
     public static void assertStatusCode(Response response) {
-        Assert.assertEquals(response.statusCode(), 201);
+        Allure.step("Check status code is 200", () -> {
+            Assert.assertEquals(response.statusCode(), 201);
+        });
     }
 
-    @Step("Validate success message")
     public static void assertSuccessMessage(AddBookResponse response) {
-        Assert.assertEquals(response.getMsg(), ApiMessages.BOOK_ADDED_SUCCESS);
+        Allure.step("Verify success message", () -> {
+            Assert.assertEquals(
+                    response.getMsg(),
+                    ApiMessages.BOOK_ADDED_SUCCESS
+            );
+        });
     }
 
-    @Step("Validate Book ID is generated")
     public static void assertBookId(AddBookResponse response) {
-        Assert.assertNotNull(response.getID());
+        Allure.step("Verify Book ID is generated", () -> {
+            Assert.assertNotNull(response.getID());
+        });
     }
 
     public static void assertBookAddedSuccessfully(Response response) {
+
         assertStatusCode(response);
-        AddBookResponse res = response.as(AddBookResponse.class);
+
+        AddBookResponse res =
+                response.as(AddBookResponse.class);
+
         assertSuccessMessage(res);
         assertBookId(res);
     }
